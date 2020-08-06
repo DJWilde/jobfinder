0579be3ade70426fb6adad901cd4eefe929626b5
DJWilde <patryk.kaczor2609@gmail.com>
2020-08-06 17:37:46 +0200

    Zrobiono system alertów

--- client/src/App.js
+++ client/src/App.js
@@ -6,27 +6,37 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import RegisterEmployer from "./components/auth/RegisterEmployer";
 import LoginEmployer from "./components/auth/LoginEmployer";
+import Alert from "./components/layout/Alert";
+import { Provider } from "react-redux";
+import store from "./store";
 import "./App.css";
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path="/" component={MainPage} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/rejestracja" component={Register} />
-          <Route exact path="/logowanie" component={Login} />
-          <Route
-            exact
-            path="/pracodawcy/rejestracja"
-            component={RegisterEmployer}
-          />
-          <Route exact path="/pracodawcy/logowanie" component={LoginEmployer} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={MainPage} />
+        <section className="container">
+          <Alert />
+          <Switch>
+            <Route exact path="/rejestracja" component={Register} />
+            <Route exact path="/logowanie" component={Login} />
+            <Route
+              exact
+              path="/pracodawcy/rejestracja"
+              component={RegisterEmployer}
+            />
+            <Route
+              exact
+              path="/pracodawcy/logowanie"
+              component={LoginEmployer}
+            />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
